877b85c698ec739f2dda4d1266f090d44949c55c
Bartek <bartekgryczka925@gmail.com>
2026-06-11 11:02:50 +0200

    update do bazy
    zapomnialem wczesniej

--- alejenews.sql
+++ alejenews.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Cze 08, 2026 at 05:45 PM
+-- Generation Time: Cze 11, 2026 at 11:02 AM
 -- Wersja serwera: 10.4.32-MariaDB
--- Wersja PHP: 8.2.12
+-- Wersja PHP: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,21 +29,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `news` (
   `id` int(11) NOT NULL,
-  `zdjecie` varchar(255) NOT NULL,
+  `plik` varchar(255) NOT NULL,
   `tytul` varchar(255) NOT NULL,
   `opis` text NOT NULL,
-  `czas_trwania` int(11) NOT NULL DEFAULT 5
+  `czas_trwania` int(11) NOT NULL DEFAULT 5,
+  `rodzaj` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `news`
 --
 
-INSERT INTO `news` (`id`, `zdjecie`, `tytul`, `opis`, `czas_trwania`) VALUES
-(1, 'domanski.jpg', 'Mikolaj Domanski z srednia 1.0', 'Nikt nie wierzy, ale to prawda. Chlopak placze kazdy w szoku, dyrektor nie wie co zrobic.', 3),
-(2, 'bomba.avif', 'Bomba w bolierze', 'Ktos mowil ze w bolierze jest bomba, nikt nie dowierzal a 4 chlopakow na palarni wyjebalo w powietrze', 3),
-(3, 'eryczek.jpg', 'Wielka ryba zlowiona przez ucznia Alej AZ 3KG !', 'Ledwo utrzymal w rekach, ale takiego brania dawno nie bylo', 3),
-(4, 'dyzio.png', 'Potwor grasuje po Łukowie', 'Prosimy pansta o uwazanie na powyzszego drapieznika, ma okolo 10m wzrostu i zabija spojrzeniem', 5);
+INSERT INTO `news` (`id`, `plik`, `tytul`, `opis`, `czas_trwania`, `rodzaj`) VALUES
+(1, 'domanski.jpg', 'Mikolaj Domanski z srednia 1.0', 'Nikt nie wierzy, ale to prawda. Chlopak placze kazdy w szoku, dyrektor nie wie co zrobic.', 3, 'zdjecie'),
+(2, 'bomba.avif', 'Bomba w bolierze', 'Ktos mowil ze w bolierze jest bomba, nikt nie dowierzal a 4 chlopakow na palarni wyjebalo w powietrze', 3, 'zdjecie'),
+(3, 'eryczek.jpg', 'Wielka ryba zlowiona przez ucznia Alej AZ 3KG !', 'Ledwo utrzymal w rekach, ale takiego brania dawno nie bylo', 3, 'zdjecie'),
+(4, 'dyzio.png', 'Potwor grasuje po Łukowie', 'Prosimy pansta o uwazanie na powyzszego drapieznika, ma okolo 10m wzrostu i zabija spojrzeniem', 5, 'zdjecie');
 
 --
 -- Indeksy dla zrzutów tabel
